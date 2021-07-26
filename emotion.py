54776f1ddfd1a527f2df2fd3d1b1e593b47baa4d
Frost-fusion <prashisborkar1999@gmail.com>
2021-07-26 14:25:55 +0530

    rebase

---
+++ emotion.py
@@ -0,0 +1,73 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.python.keras.engine import base_layer_v1
+
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os
+from display import Display
+
+# gpus = tf.config.list_physical_devices('GPU')
+try:
+    tf_gpus = tf.config.list_physical_devices('GPU')
+    for gpu in tf_gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+except:
+    pass
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+mode = "display"
+
+# Define data generators
+
+# emotions will be displayed on your face from the webcam feed
+if __name__ == '__main__':
+    train_dir = 'data/train'
+    val_dir = 'data/test'
+
+    num_train = 28709
+    num_val = 7178
+    batch_size = 64
+    num_epoch = 50
+
+    train_datagen = ImageDataGenerator(rescale=1. / 255)
+    val_datagen = ImageDataGenerator(rescale=1. / 255)
+
+    train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(48, 48),
+        batch_size=batch_size,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+    validation_generator = val_datagen.flow_from_directory(
+        val_dir,
+        target_size=(48, 48),
+        batch_size=batch_size,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+    # Create the model
+    model = Sequential()
+
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(1024, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(7, activation='softmax'))
+
+    model.load_weights('model.h5')
+    Display(model)
